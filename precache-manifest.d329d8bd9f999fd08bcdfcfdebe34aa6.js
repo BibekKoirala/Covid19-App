03fd177a735fd85f2c6aaa7b018a278392004fa4
Bibek Koirala <bibek.high@gmail.com>
2020-06-29 03:52:09 +0530

    Updates

---
+++ precache-manifest.d329d8bd9f999fd08bcdfcfdebe34aa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07191f9f85c19f26ca40ffca5b42a43a",
+    "url": "/Covid19-App/index.html"
+  },
+  {
+    "revision": "3ed54759f6827cb98272",
+    "url": "/Covid19-App/static/css/main.4259514b.chunk.css"
+  },
+  {
+    "revision": "2d5bc826c01095e09c6a",
+    "url": "/Covid19-App/static/js/2.0fd3fb57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Covid19-App/static/js/2.0fd3fb57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed54759f6827cb98272",
+    "url": "/Covid19-App/static/js/main.fb8c4695.chunk.js"
+  },
+  {
+    "revision": "a5b66f2552bb5e1def4a",
+    "url": "/Covid19-App/static/js/runtime-main.90840217.js"
+  }
+]);
